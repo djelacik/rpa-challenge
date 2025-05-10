10a4ee131979381aaf93282fbd56a8195cc7c41f
Daniel Jelacik <danieljelacik@Tamaras-MacBook-Air.local>
2025-05-10 18:52:44 +0200

    feat: add function to load excel data as list of dictionarys

---
+++ data_loader.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from typing import Dict, List
+
+def excel_to_dict(filename: str) -> List[Dict]:
+    dataframe = pd.read_excel(filename)
+    #orient="records" to use column names as keys
+    dataframe_dict = dataframe.to_dict(orient="records")
+    return dataframe_dict
