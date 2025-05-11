a913879e1618071b00e4aeb57cc4cddf4b149bb1
Daniel Jelacik <daniel.jelacik@gmail.com>
2025-05-11 19:07:24 +0200

    feat(cli): add --headless & --fast flags to control browser and performance modes
    Implemented CLI flags using argparse for headless mode or perfomance mode
    --headless mode hides UI display
    --fast mode hides display too and calculates more accurate form fill time

--- form_automation.py
+++ form_automation.py
@@ -4,6 +4,14 @@ from playwright.async_api import Page
 import asyncio
 import pandas as pd
 from typing import List, Dict
+import time
+import argparse
+
+#parse command line arguments
+parser = argparse.ArgumentParser(description="Run RPA automation challenge")
+parser.add_argument("--headless", action="store_true", help="Run in headless mode")
+parser.add_argument("--fast", action="store_true")
+args = parser.parse_args()
 
 def excel_to_dict_list(filename: str) -> List[Dict]:
     dataframe = pd.read_excel(filename)
@@ -25,20 +33,18 @@ async def fill_page(page: Page, person: Dict) -> None:
 
         #.text_content() returns what user sees, e.g. "First Name"
         label_text = (await label.text_content())
-        #label_text = (await label.text_content()).strip()
 
-        #if label == {key:}, fill with {:value}
+        #typecast everything to string, because of phone number
         if label_text in person:
             await input.fill(str(person[label_text]))
         else:
             print(f"Label doesn't match: '{label_text}'")
 
 
-
 async def main() -> None:
     async with async_playwright() as playwright:
-        #headless=False to see the rpa window for debugging
-        browser = await playwright.chromium.launch(headless=False)
+        #args.headless stores boolean value, true/false
+        browser = await playwright.chromium.launch(headless=args.headless or args.fast)
         context = await browser.new_context()
         page = await context.new_page()
 
@@ -46,6 +52,9 @@ async def main() -> None:
         await page.get_by_role("button", name="Start").click()
 
         data = excel_to_dict_list("challenge.xlsx")
+
+        start = time.time()
+
         for person in data:
             #Remove any extra spaces in Excelf headers
             person_cleaned = {key.strip(): value for key, value in person.items()}
@@ -55,7 +64,15 @@ async def main() -> None:
             #await page.wait_for_timeout(1000)
             #wait for next form to load for 500 milliseconds
 
-        input()
+        end = time.time()
+        #print own timer in perfomance mode
+        if args.fast:
+            print(f"\nPython timer: {int((end - start)*1000)} ms")
+        
+        #save and print the result to user
+        result_text = await page.locator("div.message2").text_content()
+        print(result_text)
+
         #close tab
         await context.close()
         #clear cookies etc.
