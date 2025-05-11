ca2b692a2c679b380055b7694cafc962b23da25e
Daniel Jelacik <daniel.jelacik@gmail.com>
2025-05-11 19:54:40 +0200

    fix: move data_loader to correct folder

--- data_loader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from typing import Dict, List
-
-def excel_to_dict(filename: str) -> List[Dict]:
-    dataframe = pd.read_excel(filename)
-    #orient="records" to use column names as keys
-    dataframe_dict = dataframe.to_dict(orient="records")
-    return dataframe_dict
